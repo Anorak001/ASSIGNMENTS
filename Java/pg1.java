0c30b12d66ff8b4bfb0b44de5bf962a5aaf33df6
MANTHAN R M <122231661+Anorak001@users.noreply.github.com>
2024-04-15 23:39:33 +0530

    Update pg1.java

--- Java/pg1.java
+++ Java/pg1.java
@@ -1,1 +1,14 @@
+package lab;
 
+public class pg1 {
+
+	public static void main(String[] args) {
+		//program to calculate sum of all elements in an array
+		int a[]= {1,2,3,4,5};
+		int sum=0;
+		for(int i=0;i<5;i++)
+		{sum=sum+a[i];}
+		System.out.println("sum of values is "+ sum);
+	
+
+}}
